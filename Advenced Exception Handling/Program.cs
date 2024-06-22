3153743bfbe486d3e740b818de24be96b125092a
Soner Koçak <Soner Koak@SONER>
2024-06-23 01:18:32 +0300

    Proje dosyası ekle.

---
+++ Advenced Exception Handling/Program.cs
@@ -0,0 +1,91 @@
+﻿using Advenced_Exception_Handling;
+using System;
+using System.Threading.Tasks;
+
+
+namespace Exception_Handling
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            HandleException(() =>
+            {
+                Find();
+            });
+
+        }
+
+        private static void HandleException(Action action)
+        {
+            try
+            {
+                action.Invoke();
+            }
+
+            catch (RecordNotFoundException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            //Bazı yaygın exceptionlar
+            catch (ArgumentNullException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            catch (IndexOutOfRangeException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            catch (InvalidOperationException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            catch (NullReferenceException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            catch (DivideByZeroException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            catch (FileNotFoundException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            catch (FormatException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            catch (OutOfMemoryException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+        }
+
+
+        public static void Find()
+        {
+            List<string> nameList = new List<string>() { "ahmet", "mehmet", "ayşe", "fatma" };
+
+            if (!nameList.Contains("mohammad"))
+            {
+                throw new RecordNotFoundException(message: "kayıt yok");// özel hata mesajı
+            }
+
+            else
+            {
+                Console.WriteLine("mohammad kaydı mevcut.");
+            }
+        }
+    }
+}
